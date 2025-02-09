8144949c37145fa83ea9adb84f6c9753fe3ebebf
Diego <diegol-patinof@unilibre.edu.co>
2025-02-09 18:31:22 -0500

    Se agregan ejercicios simples de java

---
+++ java/Main.java
@@ -0,0 +1,17 @@
+import java.util.*; //Esto es un comentario
+
+/*
+Otro comentario 
+en 3 lineas
+mi socio
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		System.out.println(" Hello World");
+
+	}
+}
+ 
---
+++ java/aritmetica.java
@@ -0,0 +1,20 @@
+public class aritmetica {
+    public static void main(String[] args){
+        int numero1, numero2;
+
+        numero1 = 15;
+        numero2 = 9;
+
+        //suma
+        System.out.println(numero1 + " + " + numero2 + " = " + (numero1+numero2));
+
+        //resta
+        System.out.println(numero1 + " - " + numero2 + " = " + (numero1-numero2));
+
+        //multiplicacion
+        System.out.println(numero1 + " * " + numero2 + " = " + (numero1*numero2));
+
+        //division
+        System.out.println(numero1 + " / " + numero2 + " = " + (numero1/numero2));
+    }
+}
