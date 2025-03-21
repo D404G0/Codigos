a20b33e26940f0ad9f29a2f181a9609df8636794
Diego Patiño <diegol-patinof@unilibre.edu.co>
2025-03-21 10:41:55 -0500

    ejercicios de herencia

--- java/herencia.java
+++ java/herencia.java
@@ -1,5 +1,5 @@
 //herencia
-import java.util: *;
+import java.util. *;
 
 //super clase
 class persona{
@@ -17,30 +17,44 @@ class persona{
     
     //Método para mostrar informacion
     public void mostrar(){
-        System.out.println("Nombre " + nombre)
-        System.out.println("edad " + edad)
+        System.out.println("Nombre: " + nombre);
+        System.out.println("edad: " + edad);
     }
 }
 
 //subclase que hereda
 class estudiante extends persona{
-    String uni;
+    String uni, carrera;
     
     //constructor estudiante
-    public estudiante(String nombre, uni; int edad){
+    public estudiante(String nombre, int edad, String uni, String carrera){
         super(nombre, edad); 
         this.uni = uni;
+        this.carrera = carrera;
     }
     
     public void mostrar_uni(){
-        System.out.println("Universidad" + uni);
+        System.out.println("Universidad: " + uni);
+        System.out.println("Carrera: " + carrera);
     }
 }
 
+
 class Main{
-    public class void main(String[] args){
+    public static void main(String[] args){
+        System.out.println("Herencia");
+        
+        estudiante estu = new estudiante("Karol", 19, "Universidad Los Libertadores", "Hoteleria y turismo");
+        
+        estudiante estu_dos = new estudiante("Diego", 19, "Universidad Libre", "Ingenieria en sistemas");
+        
+        estu.mostrar();
+        estu.mostrar_uni();
+        
+        System.out.println(" ");
+        
+        estu_dos.mostrar();
+        estu_dos.mostrar_uni();
         
     }
 }
-
- 
